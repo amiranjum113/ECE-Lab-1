ae601ce7290630fb6a533438ea42ada2de15e3c0
DINOREX <225062012+amiranjum113@users.noreply.github.com>
2026-02-09 19:49:37 -0700

    "Final Commit"

--- src/thirtyOneDayMonth.vhd
+++ src/thirtyOneDayMonth.vhd
@@ -55,38 +55,25 @@ library ieee;
 -- entity name should match filename, this has been filled out for you  
 entity thirtyOneDayMonth is 
   port(
-	i_A :  in std_logic; -- one of four inputs
-	i_B :  in std_logic;
-	i_C :  in std_logic;
-	i_D :  in std_logic;
-						
-	o_Y :  out std_logic					-- output
+	i_A : in std_logic; -- one of four inputs
+	
+	
+	
+						-- output
   );
 end thirtyOneDayMonth;
 
 architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is 
 	-- include components declarations and signals
-
+	
 	--signals internal to the architecture are declared and initialized such as w_sel
-    signal w_sel : std_logic_vector (2 downto 0); -- MUX sel
+  
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
 	w_sel(0) <= i_C;	-- one
-	w_sel(2) <= i_A;
-	w_sel(1) <= i_B;
 	--finish assigning signals
 	
-	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.
-	with w_sel select
-	o_Y <= i_D when "000",
-	       i_D when "001",
-	       i_D when "010",
-	       i_D when "011",
-	       not i_D when "100",
-	       not i_D when "101",
-	       not i_D when "110",
-	       not i_D when "111",
-	       '0' when others;
+	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
